127a9bfcac85c52b76e313f6250c21afa99ee724
adarshdev26 <Adarshkatoch82@gmail.com>
2025-05-24 12:34:10 +0530

    first commit
    navigation setup and native elements

---
+++ app/index.jsx
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet, Image } from 'react-native'
+import React from 'react';
+import logo from '../assets/images/logo.jpg';
+import { Link } from 'expo-router';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+
+      <Image source={logo} alt='logo' style={styles.img} />
+
+      <Text style={styles.title}>The Number 1</Text>
+      <Text style={{marginTop:10, marginBottom:10}}>Reading List App</Text>
+      <Link href='/about' style={styles.link}>About Page</Link>
+      <Link href='/contact' style={styles.link}>Contact Page</Link>
+    </View>
+  )
+}
+
+export default Home
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  title:{
+    fontSize:18,
+    fontWeight:'bold'
+  },
+  img:{
+    marginVertical:10
+  },
+  link:{
+    marginVertical:10,
+    borderBottomWidth:1
+  }
+})
+
